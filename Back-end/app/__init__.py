fb3955ba051bbd66676a47b559b6c39f67cb4457
PabloValcarce <pvalca99@gmail.com>
2025-01-14 16:47:51 +0100

    environment variables creation and implementation

--- Back-end/app/__init__.py
+++ Back-end/app/__init__.py
@@ -2,21 +2,24 @@ from flask import Flask
 from flask_cors import CORS
 from app.models import db
 from config import Config
+from dotenv import load_dotenv
+import os
+
+load_dotenv()  # Cargar variables de entorno desde el archivo variables.env
 
 def create_app():
     app = Flask(__name__)
-    
     app.config.from_object(Config)
-    
+
     db.init_app(app)
-    CORS(app)  
-    
+    CORS(app)  # Habilitar CORS para toda la aplicación
+
+    # Registrar rutas
     from app.routes.main_routes import main
     from app.routes.auth_routes import auth
     from app.routes.transaction_routes import transactions
-
     app.register_blueprint(main)
     app.register_blueprint(auth, url_prefix='/auth')
-    app.register_blueprint(transactions, url_prefix='/api/transactions') 
+    app.register_blueprint(transactions, url_prefix='/api/transactions')
 
     return app
--- Back-end/config.py
+++ Back-end/config.py
@@ -1,8 +1,19 @@
 import os
+import logging
 
 class Config:
+    # Extraer valores de las variables de entorno
+    SECRET_KEY = os.getenv('SECRET_KEY', 'default_secret_key')  # Clave secreta predeterminada
+    DB_USER = os.getenv('DB_USER', 'default_user')
+    DB_PASSWORD = os.getenv('DB_PASSWORD', 'default_password')
+    DB_HOST = os.getenv('DB_HOST', 'localhost')
+    DB_PORT = os.getenv('DB_PORT', '5432')
+    DB_NAME = os.getenv('DB_NAME', 'default_db')
+
+    # Configurar la URI de la base de datos
     SQLALCHEMY_DATABASE_URI = (
-        f"postgresql://{os.getenv('DB_USER', 'postgres')}:{os.getenv('DB_PASSWORD', 'mel')}@"
-        f"{os.getenv('DB_HOST', 'localhost')}:{os.getenv('DB_PORT', '5432')}/{os.getenv('DB_NAME', 'Gestor')}"
+        f"postgresql://{DB_USER}:{DB_PASSWORD}@{DB_HOST}:{DB_PORT}/{DB_NAME}"
     )
+
+    # Desactivar el seguimiento de modificaciones para SQLAlchemy
     SQLALCHEMY_TRACK_MODIFICATIONS = False
